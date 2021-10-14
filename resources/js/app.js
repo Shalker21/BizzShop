f71a7bc93654f00403f8911e4c2e94359777688f
David <rac.david.salamon@gmail.com>
2021-10-14 12:39:01 +0200

    npm_run_watch, created test vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+import { createApp } from 'vue'
+import HelloWorld from './components/HelloWorld.vue';
+
+const app = createApp({});
+app.component('hello-world', HelloWorld)
+    .mount('#app');
+    
 require('./bootstrap');
---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+<script>
+export default {
+    setup: () => ({
+        greeting: 'Hello World from Vue 3!'
+    })
+}
+</script>
