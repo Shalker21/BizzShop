11f6dfd7fd8ba2bdcffe8138766d844aa9d464f0
David <rac.david.salamon@gmail.com>
2021-10-14 20:27:10 +0200

    created routes for login and registration within admin prefix

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -29,7 +29,21 @@
                                     class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                             </div>
                             <div class="relative">
-                                <button class="bg-blue-500 text-white rounded-md px-4 py-2">Prijava</button>
+                                <input autocomplete="off" id="password" name="password" type="password"
+                                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                    placeholder="Password" />
+                                <label for="password"
+                                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                            </div>
+                            <div class="relative">
+                                <input autocomplete="off" id="password" name="password" type="password"
+                                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                    placeholder="Password" />
+                                <label for="password"
+                                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                            </div>
+                            <div class="relative">
+                                <button class="bg-blue-500 text-white rounded-md px-4 py-2">Registracija</button>
                             </div>
                         </div>
                     </div>
--- routes/admin.php
+++ routes/admin.php
@@ -2,3 +2,15 @@
 
 Route::view('/admin', 'admin.dashboard.index');
 Route::view('/admin/login', 'admin.auth.login');
+Route::view('/admin/register', 'admin.auth.register');
+
+// prefix means in url => /admin/...
+Route::prefix('admin')->group(function () {
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
+    Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+
+    Route::get('/', function () {
+        return view('admin.dashboard.index');
+    });
+});
