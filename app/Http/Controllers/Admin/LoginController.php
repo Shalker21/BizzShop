806b13aa3277dc741f5beb9429e8ba2334d54673
David <rac.david.salamon@gmail.com>
2021-10-18 19:12:11 +0200

    creating routes and redirecting user to success login page, need to FIX problem with routes !!!! not finished yet

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreAdminLoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -29,5 +31,37 @@ class LoginController extends Controller
 
     public function showLoginForm() {
         return view('admin.auth.login');
+    }
+
+    public function login(Request $request) {
+        //dd("TEST");
+        //$validation = $request->validated();
+
+        $this->validate($request, [
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('admin')->attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ], $request->get('remember')
+        )) {
+            // indended() => For example, I tried to view a private page, but I was redirected to login. After I logged in, i'd be redirected to my intended location (this is com from laracasts)
+            //return redirect()->indended(route('admin.dashboard.index')); // problem with indended because it redirect to /login not to /admin/login!!
+        
+            return redirect()->coute('admin.dashboard.index');
+        }
+
+        return back()->withInput($request->only('email', 'remember'));
+    }
+
+    public function logout(Request $request) {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
     }
 }
--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -12,7 +12,8 @@
                         <h1 class="text-2xl font-semibold">ADMIN PANEL</h1>
                     </div>
                     <div class="divide-y divide-gray-200">
-                        <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                        <form action="{{ route('admin.login.post') }}" method="POST" class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                            @csrf
                             <div class="relative">
                                 <input autocomplete="off" id="email" name="email" type="text"
                                     class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
@@ -29,9 +30,14 @@
                                     class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                             </div>
                             <div class="relative">
-                                <button class="bg-blue-500 text-white rounded-md px-4 py-2">Prijava</button>
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label for="remember"
+                                    class="absolute left-5 top-1 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Zapamti Me</label>
                             </div>
-                        </div>
+                            <div class="relative">
+                                <button class="bg-blue-500 text-white rounded-md px-4 py-2" type="submit">Prijava</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
