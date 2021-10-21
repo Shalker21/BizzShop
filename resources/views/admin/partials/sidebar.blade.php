49dde92877521daa4a6cbd939e2629f9e25c4a82
David <rac.david.salamon@gmail.com>
2021-10-21 23:26:38 +0200

    -

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -169,15 +169,6 @@
                         </svg>
                     </span>
                     <span class="ml-2 text-sm"> Korisnici </span>
-                    <span aria-hidden="true" class="ml-auto">
-                        <!-- active class 'rotate-180' -->
-                        <svg class="w-4 h-4 transition-transform transform" :class="{ 'rotate-180': open }"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M19 9l-7 7-7-7" />
-                        </svg>
-                    </span>
                 </a>
             </div>
         </nav>
