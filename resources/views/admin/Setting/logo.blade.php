dec9d6f09616b48aaf95e460ef92e530cf1232e0
David <rac.david.salamon@gmail.com>
2021-10-22 16:45:30 +0200

    -

--- resources/views/admin/Setting/logo.blade.php
+++ resources/views/admin/Setting/logo.blade.php
@@ -19,18 +19,20 @@
                             </button>
                         </div>
                     </div>
-                    <div class="bg-gray-800 text-white py-3 px-4 text-center fixed left-0 bottom-0 right-0 z-40">
-                        This a Tailwind File Upload by shuvro_008. 
-                        <a class="underline text-gray-200" href="https://tailwindcomponents.com/component/tailwind-file-upload">Component details</a>
-                    </div>
-                    <div class="flex w-full h-screen items-center justify-center bg-grey-lighter">
-                        <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
-                            <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                            </svg>
-                            <span class="mt-2 text-base leading-normal">Select a file</span>
-                            <input type='file' class="hidden" />
-                        </label>
+                    <div class="grid grid-cols-3 gap-4">
+                        <div class="">
+                            SITE LOGO IMAGE
+                        </div>
+                        <div class="bg-red-500">
+                            <label class="w-64 flex flex-col items-center px-4 py-6 m-5 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+                                <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                                </svg>
+                                <span class="mt-2 text-base leading-normal">Odaberi Logo web stranice</span>
+                                <input type='file' class="hidden" />
+                            </label>
+                        </div>
+                        
                     </div>
                 </form>
             </div>
