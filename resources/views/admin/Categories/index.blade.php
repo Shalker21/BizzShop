0a4e91069c1f1f8ad7a1d71444e13520a33a9b38
David <rac.david.salamon@gmail.com>
2021-10-26 18:24:01 +0200

    -

---
+++ resources/views/admin/Categories/index.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.app')
+@section('content')
+
+    <section class="bg-blueGray-50">
+        <div class="w-full lg:w-8/12 px-4 mx-auto mt-6">
+            <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+                <form action="{{ route('admin.categories.update') }}" method="POST" role="form">
+                    @csrf
+                    <div class="rounded-t bg-white mb-0 px-6 py-6 dark:bg-darker dark:text-light">
+                        <div class="text-center flex justify-between">
+                            <h6 class="text-blueGray-700 text-xl font-bold">
+                                Kategorije
+                            </h6>
+                            <button
+                                class="bg-blue-500 text-white active:bg-blue-600 hover:bg-blue-400 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                                type="submit">
+                                Spremi Promjene
+                            </button>
+                        </div>
+                    </div>
+                    <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+                        <div class="flex flex-wrap mt-9">
+                            <div class="w-full lg:w-6/12 px-4">
+                                <div class="relative w-full mb-3">
+                                    <label class="dark:text-light block uppercase text-blueGray-600 text-xs font-bold mb-2 ">
+                                        Naziv web stranice
+                                    </label>
+                                    <input type="text"
+                                        id="site_name"
+                                        name="site_name"
+                                        class="dark:text-gray-600 border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                                        value="{{ config('settings.site_name')}}">
+                                </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
