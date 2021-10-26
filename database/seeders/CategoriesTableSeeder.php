b1632c024ea741a06a9d76c0cceaf8ec5c02db1c
David <rac.david.salamon@gmail.com>
2021-10-26 20:49:39 +0200

    creating seeder for category and successed populate database, now I need to create store function and work on that

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -15,12 +17,16 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         Category::create([
-            'name'          =>  'Root',
-            'description'   =>  'This is the root category, don\'t delete this one',
-            'parent_id'     =>  null,
-            'menu'          =>  0,
+            'parent_id' =>  null,
+            'featured' => false,
+            'menu' =>  false,
         ]);
 
-        factory('App\Models\Category', 10)->create();
+        DB::table('category_translation')->insert([
+            'category_id' => Category::first()->id,
+            'name' => 'Root',
+            'slug' => 'root',
+            'description' => Str::random(20), 
+        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(AdminTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
     }
 }
