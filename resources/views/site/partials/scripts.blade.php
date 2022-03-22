683b6b504736d4bae68f402b797b369f203a95e6
David <rac.david.salamon@gmail.com>
2022-03-22 20:35:00 +0100

    -

--- resources/views/site/partials/scripts.blade.php
+++
@@ -1,4 +0,0 @@
-<script src="{{ asset('js/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/js/glightbox.min.js') }}"></script>
-<script src="{{ asset('js/js/main.js') }}"></script>
-<script src="{{ asset('js/js/tiny-slider.js') }}"></script>
